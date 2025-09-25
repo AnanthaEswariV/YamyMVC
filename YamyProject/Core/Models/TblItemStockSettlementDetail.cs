9aeccf499b764a5333118251ef2564abc3da5895
sakher1-1 <sakher@yamyco.co>
2025-09-25 17:05:48 +0400

    Edite The companyCenter Edite The Enventoey Model add The View Model Add Service For Enventoey Edite the Main Layout Edite the Global

--- YamyProject/Core/Models/TblItemStockSettlementDetail.cs
+++ YamyProject/Core/Models/TblItemStockSettlementDetail.cs
@@ -7,9 +7,18 @@ public partial class TblItemStockSettlementDetail
 {
     public int Id { get; set; }
 
+    [Column("id")]
     public int? SettleId { get; set; }
 
+    [ForeignKey(nameof(SettleId))]
+    public virtual TblItemStockSettlement Settlement { get; set; }
+
+
     public int? ItemId { get; set; }
+    [ForeignKey(nameof(ItemId))]
+
+    public TblItem Item { get; set; }
+
 
     public decimal? OnHand { get; set; }
 
@@ -20,6 +29,7 @@ public partial class TblItemStockSettlementDetail
     public decimal? Qty { get; set; }
 
     public decimal? Minusamount { get; set; }
-
+    public int MinusAmount { get; internal set; }
     public decimal? Plusamount { get; set; }
+    public int PlusAmount { get; internal set; }
 }
